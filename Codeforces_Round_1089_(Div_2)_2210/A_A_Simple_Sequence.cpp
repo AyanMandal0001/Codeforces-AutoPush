b5c688a7fe1d1ba52114f709217220e0b0bdb8e7
AyanMandal0001 <ayanmandal9792@gmail.com>
2026-03-31 00:19:51 +0530

    Add 1 accepted solutions

---
+++ Codeforces_Round_1089_(Div_2)_2210/A_A_Simple_Sequence.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+void solve(){
+ int n ; cin >> n;
+ for(int i=n;i>=1;i--){
+  cout<<i<<" ";
+ }
+ cout<<endl;
+}
+signed main(){
+   int tt; cin >> tt; while(tt--)
+     solve();
+}
